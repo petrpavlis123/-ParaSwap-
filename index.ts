97680d6f8b02757c89f572279d39f3d6fd33ba12
Petr Pavlis <petrpavlis123@gmail.com>
2024-05-04 10:42:56 +0200

    base

---
+++ index.ts
@@ -0,0 +1,1 @@
+export { bridgeAndSwap } from "./src/bridgeAndSwap"
---
+++ src/bridgeAndSwap.ts
@@ -0,0 +1,9 @@
+export function bridgeAndSwap() {
+    // Bridge USDT z AssetHub -> HydraDX
+
+    // Swap USDT -> DOT
+
+    // Bridge DOT z HydraDX -> Polkadot
+
+    // Bridge DOT z Polkadot -> AssetHub
+}

89afa8d0be29672c09d48ff136294d6261284d3b
rostislavlitovkin <rostislavlitovkin@gmail.com>
2024-05-05 21:59:28 +0200

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
 
 module.exports = {
   mode: "production",
   entry: './index.ts',
   output: {
     path: path.resolve(__dirname, 'lib'),
-    filename: 'paraswap.js',
-    library: 'ParachainSwap',
+    filename: 'parachainswap.js',
+    library: 'parachainswap',
     libraryTarget: 'umd',
     globalObject: 'this'
   },
